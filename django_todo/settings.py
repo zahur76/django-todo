9ca65f4d0fdb8131c3558d6f9c52b492ff8e8107
zahur76 <zahurmeerun@hotmail.com>
2020-11-28 17:23:40 +0000

    Correct typo

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '**#^cckd#npjvw^-wx!(%7qa*tdqux(8!c6j4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get9('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
